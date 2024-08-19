effe3c0e8e63d4a190142bb10bb3d5781f76a8cd
Pavankalyan <pavankalyanmagam@gmail.com>
2024-08-19 11:55:12 -0500

    Initial Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,20 @@
 public class Main {
+    private static String fistName = "Pavan";
+    private static String lastName = "Nani";
+    private static String College = "UCM";
+
     public static void main(String[] args) {
 
+
         System.out.println("Hello IntelliJ");
-        for(int i = 1 ; i<=10; i++) {
 
 
-            System.out.println("2 * " + i + " =" + 2*i  );
+
+
+        for (int i = 1; i <= 10; i++) {
+
+
+            System.out.println("2 * " + i + " =" + 2 * i);
         }
     }
 }
