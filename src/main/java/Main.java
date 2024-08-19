6a6523cb5002cc43491b5227e5b85bb849b6de3b
Pavankalyan <pavankalyanmagam@gmail.com>
2024-08-19 11:25:29 -0500

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello IntelliJ");
+        for(int i = 1 ; i<=10; i++) {
+
+
+            System.out.println("2 * " + i + " =" + 2*i  );
+        }
+    }
+}
